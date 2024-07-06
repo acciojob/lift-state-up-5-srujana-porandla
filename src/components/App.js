2d19fd7e58fc05bdef88caef933a81d83a27a41c
Srujana <p.srujana1356@gmail.com>
2024-07-06 14:52:48 +0530

    state

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <h1>Parent Component</h1>
         {
           isLoggedIn ? (
-            <h1>You are logged in!</h1>
+            <p>You are logged in!</p>
           ) : (
             <LoginForm onLogin={handleLogin} />
           )
